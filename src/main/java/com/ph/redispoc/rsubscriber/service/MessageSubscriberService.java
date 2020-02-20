2ca4d8bfbfa58425216aeff79fad2f6763a42ae7
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2020-02-20 11:51:05 -0300

    Adicionando data/hora ao log do subscriber

--- src/main/java/com/ph/redispoc/rsubscriber/service/MessageSubscriberService.java
+++ src/main/java/com/ph/redispoc/rsubscriber/service/MessageSubscriberService.java
@@ -1,5 +1,7 @@
 package com.ph.redispoc.rsubscriber.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
@@ -9,8 +11,7 @@ public class MessageSubscriberService implements MessageListener {
 
 	@Override
 	public void onMessage(Message message, byte[] pattern) {
-		System.out.println("Mensagem recebida: " + message.toString());
+		System.out.println("Mensagem recebida: " + message.toString() + " - " + LocalDateTime.now());
 	}
-
 	
 }
